4080ea099cf93d6737904796ad526f74bdca2d9a
Adnene Manai <adnen.manai@gmail.com>
2019-10-02 14:18:59 +0200

    Update to node 1.0.0 and support Node-red project

--- README.md
+++ README.md
@@ -3,8 +3,3 @@
 ### Deploying nodered into Azure
 
 <a href="https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Fadnmanai%2Fnode-red-azure-webapp%2Fmaster%2Fwebapp.json%3Ftoken%3D5b177ed6577cb288c3e0826134a44d961f100466" target="_blank"><img src="http://azuredeploy.net/deploybutton.png"/></a>
-
-
-### Deploying nodered into Heroku
-
-[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy?template=https://github.com/adnmanai/node-red-azure-webapp)
--- server.js
+++ server.js
@@ -1,20 +1,19 @@
-//jshint esversion:6
-var express = require("express");
-var RED=require('node-red');
-var app= express();
- var http=require('http');
+var express = require('express'),
+    RED = require('node-red'),
+    http = require('http'),
+    settings = require('./settings.js');
 
-const PORT=process.env.PORT||8000;
+var app = express(),
+    server = http.createServer(app);
 
-var server=http.createServer(app);
-var settings=require("./settings.js");
+//work around to add manually the required directory to the path in the server.js file
+process.env.Path =  'D:\\Program Files\\Git\\cmd;D:\\Program Files\\Git\\usr\\bin;' + process.env.Path;
 
 RED.init(server,settings);
 
 app.use(settings.httpAdminRoot,RED.httpAdmin);
 app.use(settings.httpNodeRoot,RED.httpNode);
  
- server.listen(settings.uiPort);
+server.listen(settings.uiPort);
 console.log(`listening port:${settings.uiPort}`);
 RED.start();
-
