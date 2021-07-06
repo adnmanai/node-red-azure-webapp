1e196c4b4b9fb8bcdde4e2a8373ef67472156099
Adnene Manai <adnen.manai@gmail.com>
2021-07-06 13:59:56 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -90,14 +90,14 @@ module.exports = {
     // -----------------
     // To password protect the Node-RED editor and admin API, the following
     // property can be used. See http://nodered.org/docs/security.html for details.
-    //adminAuth: {
-    //    type: "credentials",
-    //    users: [{
-    //        username: "admin",
-    //        password: "$2a$08$zZWtXTja0fB1pzD4sHCMyOCMYz2Z6dNbM6tl8sJogENOMcxWV9DN.",
-    //        permissions: "*"
-    //    }]
-    //},
+    adminAuth: {
+       type: "credentials",
+       users: [{
+           username: "admin",
+           password: "$2b$08$MWPvqwnxlGp21dK0doUyTeE2ALX17JFEEDhiMLKhony/x.BlRFOES",
+           permissions: "*"
+       }]
+    },
 
     // To password protect the node-defined HTTP endpoints (httpNodeRoot), or
     // the static content (httpStatic), the following properties can be used.
@@ -194,7 +194,7 @@ module.exports = {
         "template": {
           "swagger": "2.0",
           "info": {
-            "title": "Node-RED in Azure API",
+            "title": "GSC API",
             "version": "0.0.1"
           }
         }
